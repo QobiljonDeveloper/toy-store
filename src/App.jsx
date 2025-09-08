7a0c0439ce9f979df0c07803defedbbcbf18f046
QobiljonYuldashev <qobiljonyuldashev044>
2025-09-08 19:17:34 +0500

    change

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SubHero from "./components/sub-header/SubHeader";
+import SubHeader from "./components/sub-header/SubHeader";
+import Header from "./components/header/Header";
+
+const App = () => {
+  return (
+    <>
+      <SubHeader />
+      <Header />
+    </>
+  );
+};
+
+export default App;
